1c8ee4e22d0bf193a5801c19e43be10d452bbd26
Francis Pion <francispion@hotmail.com>
2025-06-09 20:19:24 -0400

    layout

---
+++ layouts/custom.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>
+      <strong>This is another layout.</strong>
+    </p>
+    <slot></slot>
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>This page is about me.</div>
+  <button @click="onClick">Click Me!</button>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'custom'
+})
+
+function onClick(): void {
+  setPageLayout('default')
+}
+</script>
